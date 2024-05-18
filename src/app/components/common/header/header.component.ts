507c33d56eaa13e6438e02f9886b040381067a8a
aren talb <aren.talb00@gmail.com>
2024-05-18 18:20:10 +0300

    Implement logout functionality

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +11,28 @@ import { AuthService } from '../../../services/auth.service';
 export class HeaderComponent {
   isAdmin = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient,
+    private router: Router,
+    private toaster: ToastrService,
+  ) {}
 
   ngOnInit() {
     this.isAdmin = this.authService.isAdmin();
+  }
+
+  logout() {
+    this.http.post('/api/user/logout', {}).subscribe(
+      (response: any) => {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+        this.toaster.success('logout success');
+      },
+      (error: any) => {
+        console.log(error?.error.message);
+        this.toaster.error(error?.error.message);
+      },
+    );
   }
 }
